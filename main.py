d084257303965f59054f5cf3db354232bba4abe6
Andres leon <189870141+Andres-1211@users.noreply.github.com>
2024-12-09 12:38:14 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#Escriba un programa que muestre la tabla de multiplicar del 1 al 10 del número ingresado por el usuario:
+
+numero = int(input("ingrese un numero para la tabla: "))
+i = 1
+while True:
+    
+    print(f"{numero} * {i} = {numero * i}")
+    i += 1
+    if i > 10:
+     break
