995d86014130d8965f643683f3fa78cc2c4117ef
Andres-1211 <189870141+Andres-1211@users.noreply.github.com>
2024-12-10 13:43:05 -0500

    solucion  6

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-#Escriba un programa que entregue todos los divisores del número entero ingresado:
+#Desarrolle un programa que permita ingresar los tiempos de viaje de los tramos y entregue como resultado el tiempo total de viaje en formato horas:minutos.
+#El programa deja de pedir tiempos de viaje cuando se ingresa un 0.
 
-numero = int(input("digite un numero "))
+def calcular_tiempo_total():
+    total_minutos = 0
+    
+    while True:
+        
+        duracion_tramo = int(input("Duracion tramo: "))
+        
+      
+        if duracion_tramo == 0:
+            break
+        
 
-
-divisores = []
-
-for i in range(1, numero +1 ):
-    if numero % i == 0:  
-        divisores.append(i)
-
-print(f"Los divisores de {numero} son: {divisores}")
-
+        
+      
+        total_minutos += duracion_tramo
+    
+  
+    horas = total_minutos // 60
+    minutos = total_minutos % 60
+    
+   
+    print(f"Tiempo total de viaje: {horas}:{minutos:} horas")
+calcular_tiempo_total()
 
